9e0fafb9d3572d1cea3e8d896c3578d0a4fc63b7
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-01-17 12:08:45 -0700

    update comments

--- unzip_n3c.py
+++ unzip_n3c.py
@@ -1,29 +1,27 @@
 #!/usr/bin/env python3
 
 '''
-
-The get_newest_payload() and extract_filenames() functions are not great and should be re-written someday.
-- tschwab
-
+  This is the way the n3c enclave unzips a file, component by component, chunk by chunk.
 '''
 import sys
 import os
 from zipfile import ZipFile
 
 # Read and write 100 MB chunks
-CHUNK_SIZE = 100 * 1024 * 1024
+#CHUNK_SIZE = 100 * 1024 * 1024
+# Read and write 1 MB chunks
+CHUNK_SIZE =  1024 * 1024
 
 
 def unzip(filename):
-    # Create a temp file to pass to zip library, because it needs to be able to .seek()
-        zipObj = ZipFile(filename)
-        for filename in zipObj.namelist():
-            with open(filename, 'wb') as out:
-                input_file = zipObj.open(filename)
+    zipObj = ZipFile(filename)
+    for filename in zipObj.namelist():
+        with open(filename, 'wb') as out:
+            input_file = zipObj.open(filename)
+            data = input_file.read(CHUNK_SIZE)
+            while data:
+                out.write(data)
                 data = input_file.read(CHUNK_SIZE)
-                while data:
-                    out.write(data)
-                    data = input_file.read(CHUNK_SIZE)
 
 if __name__ == "__main__":
     if (len(sys.argv) > 1):
