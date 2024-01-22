08eaab7799d6ac07344a424c817bf8762553486e
chrisroederucdenver <47036053+chrisroederucdenver@users.noreply.github.com>
2024-01-22 13:12:40 -0700

    more progress

--- tweak_zip.c
+++ tweak_zip.c
@@ -2,35 +2,40 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+
 #define ZIP_SIZE 490
+
 int main(int argc, char *argv[]) {
     printf("OMG\n");
     FILE *fp = fopen("good.zip", "rb");
     char *buffer = malloc(ZIP_SIZE);
     fread(buffer, ZIP_SIZE, 1, fp);
 
-    /* The offsets are at 0000424, 0000547, and  0000661 with values 0, 50 an a0.. */
-/* 276, 359, 433, now what is -8 in 2-bytes 2's compliment? 65528?? */
+    /* The offsets are at octal 0000424, 0000547, and  0000661 with values 0, 50 an a0.. */
+    /* decimal bytes: 276, 359, 433 */
+    /* divide by 4 to get ints: 69, 89.75, 110.75 */
 
-    short *short_buffer = (short *) buffer;
-    printf("%hi, %hi, %hi\n", short_buffer[138], short_buffer[179], short_buffer[217]);
+    // The offets aren't even mutliples of 4,but the numbers are ints.
 
-    //for (int i=0; i<ZIP_SIZE; i++) { 
-    //    printf("%d:   %c   %hi\n", i, buffer[i], short_buffer[i]); 
-    //}
+    // attempt 1
+    /*
+    int *bp=(int *) (buffer+359);
+    *bp=-100;
+    int *cp=(int *) (buffer+433);
+    *cp=-200;
+     //file #2:  bad zipfile offset (lseek):  4294934528
+     //file #3:  bad zipfile offset (lseek):  16769024
+    */
 
-    int i=179;
-    printf("%d:   %c   %hi\n", i, buffer[i], short_buffer[i]); 
-    i=217;
-    printf("%d:   %c   %hi\n", i, buffer[i], short_buffer[i]); 
-    short_buffer[179]=-8;
-    short_buffer[217]=-8;
-    i=179;
-    printf("%d:   %c   %hi\n", i, buffer[i], short_buffer[i]); 
-    i=217;
-    printf("%d:   %c   %hi\n", i, buffer[i], short_buffer[i]); 
+    // attempt 2
+    int *bp=(int *) (buffer+356);
+    *bp=-100;
+
+    int *cp=(int *) (buffer+430);
+    *cp=-200;
+
+
 
     FILE *wfp = fopen("gooder.zip", "wb");
-    //write(buffer, ZIP_SIZE, 1, wfp);
-    fwrite((char *) short_buffer, ZIP_SIZE, 1, wfp);
+    fwrite(buffer, ZIP_SIZE, 1, wfp);
 }
