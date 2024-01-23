5c2ac542ac01cc87e166a3c58b7d21d236627b6c
Michael Kahn <mgkahn@users.noreply.github.com>
2024-01-24 10:07:37 +1300

    Update michael.py
    Replaces N3C Python code with MGK Python code

--- michael.py
+++ michael.py
@@ -1,18 +1,23 @@
-from zipfile import ZipFile
+# Previous Python code was not from Michael Kahn.
+# This code is from Michael Kahn
+# It successfully extracts all files from an actual N3C zip file created by 7Zip
+# Including a 480GB NOTE_NLP CSV file
 
-zipfile_name = "CU-AMC_omop_2040103.zip"
+# This code extracts a ZIP file using the ZipFile.extractall() method
 
-CHUNK_SIZE = 100 * 1024 * 1024
-zipObj = ZipFile(zipfile_name)
+import zipfile
+import os
 
-for filename in zipObj.namelist():
-    file_length = 0
-    print(f"component file {filename} of zipfile {zipfile_name}")
-    input_file = zipObj.open(filename) # crashes here
-    data = input_file.read(CHUNK_SIZE)
-    file_length += len(data)
-    while data:
-        data = input_file.read(CHUNK_SIZE)
-        file_length += len(data)
-    printf(f"  Final size for {filename}: {file_length}")
+#zip_file_path = 'test.zip'
+zip_file_path = 'CU-AMC_omop_20240103.zip'
+extracted_directory = 'extracted_files'
 
+# Create the extraction directory if it doesn't exist.
+if not os.path.exists(extracted_directory):
+    os.makedirs(extracted_directory)
+
+# Open the ZIP file for reading.
+with zipfile.ZipFile(zip_file_path, 'r') as zip_ref:
+    # Extract all the files in the ZIP file to the specified directory.
+    zip_ref.extractall(extracted_directory)
+print(f'All files from {zip_file_path} have been extracted to {extracted_directory}.')
